23a782386021d8f58afac8e6f1ba378b7a63a4d6
Nanthachai.CS07 <botsafe002@gmail.com>
2024-01-31 11:12:42 +0700

    add func requestHan add path url

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/valyala/fasthttp"
 )
 
 func main() {
+	server := fasthttp.Server{
+		Handler: requestHandler,
+	}
 
+	if err := server.ListenAndServe(":8080"); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
-func testHandler(ctx *fasthttp.RequestCtx) {
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	switch string(ctx.Path()) {
+	case "/hello":
+		helloHandler(ctx)
+	default:
+		defaultHandler(ctx)
+	}
+}
+
+func helloHandler(ctx *fasthttp.RequestCtx) {
 	ctx.SetContentType("text/html")
 	fmt.Fprintf(ctx, `<doctype html>
 		<html>
@@ -20,11 +36,10 @@ func testHandler(ctx *fasthttp.RequestCtx) {
 			<body>
 				<h1>SAFECS07</h1>
 				<h2>Test go FastHTTP</h2>
-				
 			</body>
 		</html>`)
 }
 
 func defaultHandler(ctx *fasthttp.RequestCtx) {
-
+	fmt.Fprintf(ctx, "Hello, World!!!!!!!!!!!!!!!!")
 }
