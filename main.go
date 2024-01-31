915ca1cf2bdb289439c36aa14a783a393ff77eaa
Nanthachai.CS07 <botsafe002@gmail.com>
2024-01-31 11:03:20 +0700

    add create html script

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+)
+
 func main() {
 
 }
 
 func testHandler(ctx *fasthttp.RequestCtx) {
-
+	ctx.SetContentType("text/html")
+	fmt.Fprintf(ctx, `<doctype html>
+		<html>
+			<head>
+				<title>Test go FastHTTP</title>
+			</head>
+			<body>
+				<h1>SAFECS07</h1>
+				<h2>Test go FastHTTP</h2>
+				
+			</body>
+		</html>`)
 }
 
 func defaultHandler(ctx *fasthttp.RequestCtx) {
